286e0205f1be30ff4b0325ec514c4119a995aded
shadowking751 <pancakes711@icloud.com>
2026-02-19 21:13:10 -0600

    Implement client setup and entity renderer registration

---
+++ clientSetupHandler.java
@@ -0,0 +1,25 @@
+package com.yourname.gunmod.client;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraft.client.renderer.entity.ThrownItemRenderer;
+import com.yourname.gunmod.GunMod;
+import com.yourname.gunmod.entity.BulletEntity;
+
+@Mod.EventBusSubscriber(modid = GunMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientSetupHandler {
+    
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(GunAnimationHandler::registerItemProperties);
+    }
+    
+    @SubscribeEvent
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        // Use default item renderer for bullets
+        event.registerEntityRenderer(GunMod.BULLET.get(), ThrownItemRenderer::new);
+    }
+}
